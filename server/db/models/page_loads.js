2846bcd34f704f9b2340e3653db89bad87ad0396
joelcrawford <joelrootcrawford@gmail.com>
2022-12-08 17:03:59 -0500

    adding getid method to model

--- server/db/models/page_loads.js
+++ server/db/models/page_loads.js
@@ -1,24 +1,33 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+'use strict'
+const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  class page_loads extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class page_loads extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+
+        static getId(where) {
+            return this.findOne({
+                where,
+                attributes: ['userAgent'],
+                order: [['time', 'DESC']]
+            })
+        }
     }
-  }
-  page_loads.init({
-    userAgent: DataTypes.STRING,
-    time: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'page_loads',
-  });
-  return page_loads;
-};
+    page_loads.init(
+        {
+            userAgent: DataTypes.STRING,
+            time: DataTypes.DATE
+        },
+        {
+            sequelize,
+            modelName: 'page_loads'
+        }
+    )
+    return page_loads
+}
