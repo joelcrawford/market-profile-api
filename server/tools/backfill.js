65552f7ce3e42fc7db65e04d79b47ad37a17c4df
joelcrawford <joelrootcrawford@gmail.com>
2023-01-03 09:44:37 -0800

    backfill almost ready for node cron

--- server/tools/backfill.js
+++ server/tools/backfill.js
@@ -1,96 +1,61 @@
-const Candlestick = require('../dict/candlestick')
-const moment = require('moment')
 const _ = require('lodash')
-const ExchangeCandlestick = require('../dict/exchange_candlestick')
+const db = require('../db/db')
+const BinanceCandlestick = require('../dict/BinanceCandlestick')
 
 module.exports = {
-    async backfill(exchangeName, symbol, period, date) {
+    async backfill(exchangeName, endpoint, symbol, period, start, limit) {
         const exchange = exchangeName
         if (!exchange) {
             throw `Exchange not found: ${exchangeName}`
         }
 
-        let start = moment().subtract(date, 'days')
         let candles
 
         do {
-            console.log(`Since: ${new Date(start).toISOString()}`)
-            candles = await exchangeBackfill(symbol, period, start)
+            console.log(
+                `Since: ${new Date(start).toString(
+                    'YYYY-MM-DD'
+                )}, ${start}, ${typeof start}`
+            )
+            candles = await module.exports.fetchDataPromise(
+                endpoint,
+                symbol,
+                period,
+                start,
+                limit
+            )
 
-            const exchangeCandlesticks = candles.map((candle) => {
-                return ExchangeCandlestick.createFromCandle(
-                    exchangeName,
+            let dataArray = []
+            candles.forEach((e) => {
+                let k = new BinanceCandlestick(
                     symbol,
-                    period,
-                    candle
+                    '1m',
+                    e.time,
+                    e.open,
+                    e.high,
+                    e.low,
+                    e.close,
+                    e.volume
                 )
+
+                dataArray.push(k)
             })
 
-            await this.candleImporter.insertCandles(exchangeCandlesticks)
+            try {
+                db.binance_klines.bulkCreate(dataArray)
+                dataArray = []
+            } catch (error) {
+                console.log('Error inserting data', error)
+            }
 
             console.log(`Got: ${candles.length} candles`)
 
-            start = new Date(_.max(candles.map((r) => r.time)) * 1000)
+            //start = new Date(_.max(candles.map((r) => r.time))*1000)
+            // get the last candle and add a minute (update to be 1 time period)
+            start = +_.max(candles.map((r) => r.time)) + 60000
+            console.log('the next start', start)
         } while (candles.length > 10)
 
         console.log('finish')
-    },
-    exchangeBackfill(symbol, period, start) {
-        const symbolUpdated = symbol.replace('_PERP', '')
-
-        return new Promise((resolve, reject) => {
-            const query = querystring.stringify({
-                interval: period,
-                symbol: symbolUpdated,
-                limit: 500,
-                startTime: moment(start).valueOf()
-            })
-
-            request(
-                `https://dapi.binance.com/dapi/v1/klines?${query}`,
-                { json: true },
-                (err, res, body) => {
-                    if (err) {
-                        console.log(
-                            `Binance: Candle backfill error: ${String(err)}`
-                        )
-                        reject()
-                        return
-                    }
-
-                    if (res.statusCode === 429) {
-                        console.log(
-                            `Binance: Limit reached: ${String(res.headers)}`
-                        )
-                        // TODO delay next execution
-                        reject()
-                        return
-                    }
-
-                    if (!Array.isArray(body)) {
-                        console.log(
-                            `Binance: Candle backfill error: ${JSON.stringify(
-                                body
-                            )}`
-                        )
-                        reject()
-                        return
-                    }
-
-                    resolve(
-                        body.map((candle) => {
-                            return new Candlestick(
-                                moment(candle[0]).format('X'),
-                                candle[1],
-                                candle[2],
-                                candle[3],
-                                candle[4],
-                                candle[5]
-                            )
-                        })
-                    )
-                }
-            )
-        })
     }
 }
---
+++ server/tools/fetchData.js
@@ -0,0 +1,73 @@
+const moment = require('moment')
+const _ = require('lodash')
+const querystring = require('querystring')
+const https = require('https')
+const Candlestick = require('../dict/candlestick')
+
+module.exports = {
+    fetchDataPromise(endpoint, symbol, period, start, limit) {
+        return new Promise((resolve, reject) => {
+            const query = querystring.stringify({
+                interval: period,
+                symbol: symbol,
+                limit: limit,
+                startTime: start
+            })
+            console.log(`${endpoint}?${query}`)
+            //change to http for local testing
+            const request = https.request(
+                `${endpoint}?${query}`,
+                (response) => {
+                    //response.setEncoding('utf8')
+
+                    let body = []
+
+                    response.on('data', (chunk) => {
+                        body = body + chunk
+                    })
+
+                    response.on('end', () => {
+                        resolve(
+                            JSON.parse(body).map((candle) => {
+                                return new Candlestick(
+                                    moment(candle[0]).format('X'),
+                                    candle[1],
+                                    candle[2],
+                                    candle[3],
+                                    candle[4],
+                                    candle[5]
+                                )
+                            })
+                        )
+                        if (response.statusCode === 429) {
+                            console.log(
+                                `Binance: Limit reached: ${String(res.headers)}`
+                            )
+                            // TODO delay next execution
+                            reject()
+                            return
+                        }
+                        if (response.statusCode != 200) {
+                            console.log(
+                                'Api call failed with response code ' +
+                                    response.statusCode
+                            )
+                            reject()
+                            return
+                        }
+                    })
+                }
+            )
+
+            request.on('error', (e) => {
+                console.log('Error : ' + e.message)
+                reject()
+                return
+            })
+
+            // write data to request body
+            //req.write(post_data)
+            request.end()
+        })
+    }
+}
