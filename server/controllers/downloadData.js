b6a1d93cdf5cc7805975dfdb2094900dae28a57e
joelcrawford <joelrootcrawford@gmail.com>
2023-01-08 10:44:18 -0800

    starting on the download data endpoint

---
+++ server/controllers/downloadData.js
@@ -0,0 +1,40 @@
+/**
+ * GET /resources/summarize
+ * Search for a term in the library
+ * Query Params -
+ * agg_field:
+ * page_size:
+ * sort:
+ */
+
+const express = require('express')
+const { selectStats } = require('../queries/selectStats')
+
+const router = express.Router()
+
+router.get(['/:x'], async (request, response) => {
+    try {
+        //
+        console.log(request.query, request.params)
+        const body = await selectStats(request.params, request.query)
+
+        if (!body) {
+            return response.status(422).json({
+                errors: [
+                    {
+                        msg: `No matches for provided parameters, ${JSON.stringify(
+                            request.query
+                        )}`
+                    }
+                ]
+            })
+        }
+
+        // return result
+        return response.send(body)
+    } catch (error) {
+        return response.send(error)
+    }
+})
+
+module.exports = router
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-//const path = require('path')
+const path = require('path')
 //const bodyParser = require('body-parser')
 
 // Another way to do this is to create own server
@@ -29,15 +29,27 @@ server.use(
     })
 )
 
+// static files for the download endpoint
+server.use(
+    '/downloads',
+    express.static(path.join(__dirname, '..', 'public', 'temp_data'))
+)
+
 // Stats - earliest, latest dates with record count
 const getStats = require('./controllers/getStats')
+
+// MANAGE ------------------------------------
 // Duplicates - you can view count or just delete them all ?action=view,delete
 const getDupes = require('./controllers/getDupes') // Duplicates
 // Backfill - backfill data from latest date
 const postData = require('./controllers/postData')
 
+// DOWNLOAD DATA -----------------------
+const downloadData = require('./controllers/downloadData')
+
 server.use('/stats', getStats)
 server.use('/dupes', getDupes)
 server.use('/backfill', postData)
+server.use('/download', downloadData)
 
 module.exports = server
